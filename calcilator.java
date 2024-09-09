8d1a309e9f65727a0ce13bef8bf68a1e55516116
Anujsharma1399 <140787893+Anujsharma1399@users.noreply.github.com>
2024-09-09 08:42:27 -0700

    Add files via upload

---
+++ calcilator.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class SimpleCalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the first number:");
+        double num1 = scanner.nextDouble();
+
+        System.out.println("Enter the operation (+, -, *, /):");
+        char operation = scanner.next().charAt(0);
+
+        System.out.println("Enter the second number:");
+        double num2 = scanner.nextDouble();
+
+        switch (operation) {
+            case '+':
+                System.out.println("Result: " + (num1 + num2));
+                break;
+            case '-':
+                System.out.println("Result: " + (num1 - num2));
+                break;
+            case '*':
+                System.out.println("Result: " + (num1 * num2));
+                break;
+            case '/':
+                if (num2 != 0) {
+                    System.out.println("Result: " + (num1 / num2));
+                } else {
+                    System.out.println("Error: Division by zero is not allowed.");
+                }
+                break;
+            default:
+                System.out.println("Error: Invalid operation.");
+        }
+    }
+}
